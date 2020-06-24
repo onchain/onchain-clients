06c787fb1990e0a7ebb811d20454bc1a3736e895
Ian Purton <ian.purton@gmail.com>
2020-06-24 12:01:10 +0000

    Gen latest code.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Set the verison in config.json then run build.sh
+
+To download the jar
+
+`curl -L https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.4.14/swagger-codegen-cli-2.4.14.jar -o swagger-codegen.jar`
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-curl -O https://onchain.io/docs/onchain.swagger.json
+#curl -O https://onchain.io/docs/onchain.swagger.json
 
 rm -rf onchain-ruby
 
@@ -6,6 +6,6 @@ rm -rf onchain-ruby
 sed 's/onchain\.//g' onchain.swagger.json > onchain.no-packages.json
 
 # curl -O https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/3.0.0-rc1/swagger-codegen-cli-3.0.0-rc1.jar
-java -jar swagger-codegen-cli-2.4.13-20200202.173945-3.jar generate -i onchain.no-packages.json -c config.json -l ruby -o onchain-ruby
+java -jar swagger-codegen.jar generate -i onchain.no-packages.json -c config.json -l ruby -o onchain-ruby
 
 rm onchain.no-packages.json
